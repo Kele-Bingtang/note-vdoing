77e6d516d054719ce16bc3389608076bc353722f
kele_Bingtang <2456019588@qq.com>
2023-07-09 23:54:11 +0800

    添加 Jenkins - DevOps 文档，修复其他文档内容

--- docs/.vuepress/nav/toolNav.ts
+++ docs/.vuepress/nav/toolNav.ts
@@ -1,13 +1,14 @@
 // 工具导航栏
 export default {
-  text: '工具', link: '/tool/',
+  text: '工具',
+  link: '/tool/',
   items: [
     {
       text: '管理',
       items: [
         { text: 'Maven', link: '/maven/' },
-        { text: 'Git', link: '/git/' }
-      ]
+        { text: 'Git', link: '/git/' },
+      ],
     },
     {
       text: '部署',
@@ -15,7 +16,8 @@ export default {
         { text: 'Linux', link: '/linux/' },
         { text: 'Docker', link: '/docker/' },
         { text: 'Jenkins', link: '/jenkins/' },
-      ]
+        { text: 'Kubernetes', link: '/kubernetes/' },
+      ],
     },
-  ]
-}
+  ],
+};
